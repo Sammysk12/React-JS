bf39535e29f8eea72eb2a5b3943e7356fc7e2608
Sammysk12 <samarthkalshetti43@gmail.com>
2024-10-26 14:22:52 +0530

    Started with react.js and learned basic file struture of react.js and learnedc about JSX

---
+++ App_1/app-1/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+function MyComponent(){
+  const name = "Samarth Kalshetti";
+  const link = "http://samkporfolio.wuaze.com/"
+  return (
+    // <h2>Hello {name}</h2>
+    <div>
+    <h2>The sum is {Add(10,5)}</h2>
+    <h2>Click here to go to my website <a href={link}>Click here</a> </h2>
+    </div>
+  )
+}
+
+function Add(x,y){
+  return x+y;
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MyComponent/>   
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
